4f7c40afdb8eec2c86976ba46bcfa476085ebd2b
abir <akaskhan3434@gmail.com>
2023-12-26 12:28:01 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './index.css'
+import { AppProvider } from './context/Context';
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from "./pages/About/About";
+import BookList from "./components/BookList/BookList";
+import BookDetails from "./components/BookDetails/BookDetails";
+
+const App = () => {
+  return (
+    <AppProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="about" element={<About />} />
+            <Route path="book" element={<BookList />} />
+            <Route path="/book/:id" element={<BookDetails />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AppProvider>
+  );
+};
+
+export default App;
